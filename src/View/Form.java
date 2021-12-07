ebf346caf1e583dba0ef3475ebc6502dc72d51b1
Xavier Alegre Godoy <alegrexavier@gmail.com>
2021-12-07 12:08:33 +0100

    [TEFT-13] add mid separator and new buttons

--- src/View/Form.java
+++ src/View/Form.java
@@ -8,19 +8,26 @@ public class Form extends JPanel {
     public JButton selectFileButton;
     private JTextArea textField;
     private JButton selectDataButton;
+    private JButton processDatabutton;
+    private JButton saveDataButton;
 
     public Form() {
 
         JLabel textLabel = new JLabel("Text:");
         JLabel dataLabel = new JLabel("Data: "); //TODO CHANGE NAME
-        JSeparator separator = new JSeparator(SwingConstants.VERTICAL);
-        textField = new JTextArea(20, 20);
+        JSeparator separatorMid = new JSeparator(SwingConstants.VERTICAL);
+        separatorMid.setPreferredSize(new Dimension(20, 0));
+        textField = new JTextArea(30, 30);
         selectFileButton = new JButton("Select File");
         selectFileButton.setPreferredSize(new Dimension(278, 40));
         selectDataButton = new JButton("Select Data File");
         selectDataButton.setPreferredSize(new Dimension(278, 40));
+        processDatabutton = new JButton("Process");
+        processDatabutton.setPreferredSize(new Dimension(278, 40));
+        saveDataButton = new JButton("Save");
+        saveDataButton.setPreferredSize(new Dimension(270, 40));
 
-        Insets inset = new Insets(10, 10, 10, 10);
+        Insets inset = new Insets(5, 5, 5, 5);
 
         // uses Grid Bag Layout
         setLayout(new GridBagLayout());
@@ -28,11 +35,10 @@ public class Form extends JPanel {
         gridBagConstraints.weightx = 1.0;
         gridBagConstraints.weighty = 1.0;
         gridBagConstraints.insets = inset;
-        gridBagConstraints.fill = GridBagConstraints.NONE;
+
 
         gridBagConstraints.gridx = 0;
         gridBagConstraints.gridy = 0;
-        gridBagConstraints.anchor = GridBagConstraints.CENTER;
         add(textLabel, gridBagConstraints);
 
         gridBagConstraints.gridx = 0;
@@ -45,15 +51,35 @@ public class Form extends JPanel {
 
         gridBagConstraints.gridx = 1;
         gridBagConstraints.gridy = 0;
-        add(separator, gridBagConstraints);
+        gridBagConstraints.gridheight = 5;
+        gridBagConstraints.gridwidth = 1;
+        gridBagConstraints.fill = GridBagConstraints.VERTICAL;
+        add(separatorMid, gridBagConstraints);
 
         gridBagConstraints.gridx = 2;
         gridBagConstraints.gridy = 0;
+        gridBagConstraints.gridheight = 1;
+        gridBagConstraints.gridwidth = 1;
+        gridBagConstraints.fill = GridBagConstraints.NONE;
         add(dataLabel, gridBagConstraints);
 
         gridBagConstraints.gridx = 2;
         gridBagConstraints.gridy = 1;
+        gridBagConstraints.gridheight = 1;
+        gridBagConstraints.gridwidth = 1;
         add(selectDataButton, gridBagConstraints);
+
+        gridBagConstraints.gridx = 2;
+        gridBagConstraints.gridy = 2;
+        gridBagConstraints.gridheight = 1;
+        gridBagConstraints.gridwidth = 1;
+        add(processDatabutton, gridBagConstraints);
+
+        gridBagConstraints.gridx = 2;
+        gridBagConstraints.gridy = 3;
+        gridBagConstraints.gridheight = 1;
+        gridBagConstraints.gridwidth = 1;
+        add(saveDataButton, gridBagConstraints);
     }
 
     public void selectFile(ActionListener actionListener) {
--- src/View/MainFrame.java
+++ src/View/MainFrame.java
@@ -32,7 +32,7 @@ public class MainFrame extends JFrame {
         int FRAME_HEIGHT = 700;
         // size of our application frame
         setSize(FRAME_WIDTH, FRAME_HEIGHT);
-        setMinimumSize(new Dimension(500, 500));
+        setMinimumSize(new Dimension(800, 800));
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setVisible(true);
     }
